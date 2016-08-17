7748d16cf366478dfc4d7bd65caed5686b53a72a
vanbarlaur <vanbarlaur@vanbarlaur-VirtualBox>
2016-08-17 13:44:25 +1200

    Finished patterns

--- ConwaysGameOfLife/src/Backend/Pattern.java
+++ ConwaysGameOfLife/src/Backend/Pattern.java
@@ -3,7 +3,7 @@ package Backend;
 public class Pattern {
 	
 	public static int[][] getPattern(String pattern, int x, int y) {
-		int[][] newPattern = null;
+		int[][] newPattern;
 		
 		switch (pattern) {
 			case "block":
@@ -51,7 +51,12 @@ public class Pattern {
 				break;
 				
 			case "pulsar":
-				newPattern = new int[][] {{x+2, y}, {x+3, y}, {x+4, y}, {x+8, y}, {x+9, y}, {x+10, y}, {x, y+2}, };
+				newPattern = new int[][]  { {x+2, y}, {x+3, y}, {x+4, y}, {x+8, y}, {x+9, y}, {x+10, y}, {x, y+2}, {x+5, y+2}, {x+7, y+2}, {x+12, y+2}, {x, y+3}, {x+5, y+3},
+											{x+7, y+3}, {x+12, y+3}, {x, y+4}, {x+5, y+4}, {x+7, y+4}, {x+12, y+4}, {x+2, y+5}, {x+3, y+5}, {x+4, y+5}, {x+8, y+5}, {x+9, y+5},
+											{x+10, y+5}, {x+2, y+7}, {x+3, y+7}, {x+4, y+7}, {x+8, y+7}, {x+9, y+7}, {x+10, y+7}, {x, y+8}, {x+5, y+8}, {x+7, y+8}, {x+12, y+8},
+											{x, y+9}, {x+5, y+9}, {x+7, y+9}, {x+12, y+9}, {x, y+10}, {x+5, y+10}, {x+7, y+10}, {x+12, y+10}, {x+2, y+12}, {x+3, y+12},
+											{x+4, y+12}, {x+8, y+12}, {x+9, y+12}, {x+10, y+12} };
+				break;
 				
 			default:
 				newPattern = null;

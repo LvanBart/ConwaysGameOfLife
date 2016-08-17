22bcde65a94890b58eddf31c8364387182099761
sloanejose <sloanejose@sloanejose-VirtualBox>
2016-08-17 12:19:54 +1200

    Unsure of this all, created class birth, unnecessary
    need to restructure

--- ConwaysGameOfLife/src/Backend/World.java
+++ ConwaysGameOfLife/src/Backend/World.java
@@ -1,5 +1,38 @@
 package Backend;
 
-public class World {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+public class World {
+	protected int x;
+	protected int y;
+	protected int[][] alive;
+	//protected ArrayList<Birth> newborn = new ArrayList<Birth>();
+	protected Set<Integer> holding;
+	
+
+	public void tobealive() {
+		
+		}
+
+	
+
+	public void searchforlife(int x1, int y1) {
+
+		for (int i = 0; i < newborn.size(); i++) {
+
+
+			}
+		}
+
+	}
+
+	public static void main(String args[]) {
+
+		World W1 = new World();
+
+	}
 }
